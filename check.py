aa206f5a0c11594fb94cd049fc8df91fd60aad69
Faiza Benzir <faiza.benzir@northsouth.edu>
2024-01-22 01:39:14 +0600

    Commit_2

--- check.py
+++
@@ -1,6 +0,0 @@
-try:
-    import imblearn
-    print("imbalanced-learn is installed.")
-    print("imbalanced-learn version:", imblearn.__version__)
-except ImportError:
-    print("imbalanced-learn is not installed.")
